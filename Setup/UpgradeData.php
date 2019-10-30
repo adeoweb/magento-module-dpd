3dc362765085cc2fef7070ec14a64aa6909141ed
Aivaras Kairevicius <aivaras@adeoweb.lt>
2019-10-30 14:47:44 +0200

    (DPD-113) [AdeoWeb_Dpd] Magento 2.2 compatibility

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace AdeoWeb\Dpd\Setup;
 
 use Magento\Framework\App\Config\ConfigResource\ConfigInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Store\Model\Store;
 
 /**
  * @codeCoverageIgnore
@@ -31,10 +33,15 @@ class UpgradeData implements UpgradeDataInterface
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         if (version_compare($context->getVersion(), '1.1.0', '<')) {
-            $this->resourceConfig->deleteConfig('carriers/dpd/classic/restrictions');
-            $this->resourceConfig->deleteConfig('carriers/dpd/pickup/restrictions');
-            $this->resourceConfig->deleteConfig('carriers/dpd/saturday/restrictions');
-            $this->resourceConfig->deleteConfig('carriers/dpd/sameday/restrictions');
+            $this->deleteConfig('carriers/dpd/classic/restrictions');
+            $this->deleteConfig('carriers/dpd/pickup/restrictions');
+            $this->deleteConfig('carriers/dpd/saturday/restrictions');
+            $this->deleteConfig('carriers/dpd/sameday/restrictions');
         }
+    }
+
+    private function deleteConfig(string $path)
+    {
+        $this->resourceConfig->deleteConfig($path, ScopeConfigInterface::SCOPE_TYPE_DEFAULT, Store::DEFAULT_STORE_ID);
     }
 }
